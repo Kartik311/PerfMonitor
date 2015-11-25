6b6743851079c838722b2ce1a662edf8ca9ae975
Kartik311 <katyayan.kartikeya@gmail.com>
2015-11-25 14:45:14 +0530

    Updated config

--- src/main/java/com/perfmon/main/App.java
+++ src/main/java/com/perfmon/main/App.java
@@ -1,6 +1,5 @@
 package com.perfmon.main;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,12 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	StringUtils.capitalize("heelo");
-        System.out.println( StringUtils.capitalize("heelo"));
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:com/perfmon/config/spring-config.xml");
         Employee emp = (Employee)ctx.getBean("employee");
         emp.setId(1);
         emp.setName("Rahul");
         emp.printdetails();
+        ((ClassPathXmlApplicationContext)ctx).close();
     }
 }
